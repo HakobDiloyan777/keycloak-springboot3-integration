2081f4501d15a1e7c7fb69740e78bdb021d9fc48
Diloyan Akob <akob.diloyan@ax-team.corp>
2023-11-23 18:34:54 +0400

    Initial commit

---
+++ src/main/java/com/example/keycloak/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.example.keycloak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorController {
+    @GetMapping("/access-denied")
+    public ModelAndView showAccessDeniedPage() {
+        return new ModelAndView("access-denied");
+    }
+
+}
